8b06d123adbba3169a59f7e6ddec5a29d6b7fd42
Lucas Marguiron <lucas.marguiron@gmail.com>
2020-07-25 19:40:19 +0200

    Ordre affichage projets home

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -45,7 +45,7 @@ class DefaultController extends AbstractController
             ]),
             'projects' => $projectRepository->findBy(
                 array(),
-                array('id' => 'desc'),
+                array('id' => 'asc'),
                 3,
                 0
             ),
